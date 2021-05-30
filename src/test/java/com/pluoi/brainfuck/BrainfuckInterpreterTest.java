f1820d65a26176ee62b22806df969f2d6c438e63
Kavatch <alex@pluoi.com>
2021-05-30 21:20:15 +0200

    Added A test

---
+++ src/test/java/com/pluoi/brainfuck/BrainfuckInterpreterTest.java
@@ -0,0 +1,35 @@
+package com.pluoi.brainfuck;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class BrainfuckInterpreterTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    public void testHelloWorld() {
+        String input = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.";
+        BrainfuckInterpreter brainfuckInterpreter = new BrainfuckInterpreter(input);
+
+        brainfuckInterpreter.start();
+
+        assert (outContent.toString()).equals("Hello World!\n");
+    }
+
+}
